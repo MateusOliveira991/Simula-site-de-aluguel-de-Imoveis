a7454cf6f4d1b416adb1c4efefb501412536ccf0
Thigo Dev <thiagomvk08@gmail.com>
2023-11-04 15:48:32 -0300

    Feat: Criação de cards em acomodações

--- reactgrupo3/src/pages/Acomodacoes/index.jsx
+++ reactgrupo3/src/pages/Acomodacoes/index.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import '../Acomodacoes/style.css'
 
 export default function Acomodacoes() {
+
+
+
   return (
-    <div>
-      <h2>Acomodações</h2>
+    <div className="Acontainer">
+      <div className='Aheader'>
+        <h2>Acomodações</h2>
+      </div>
+      <div className="Acards">
+        <div className="Acard">
+          <img src="src\assets\images\casa.jpg" alt="imagem de casa" width={200}/>
+          <h3>Título da casa</h3>
+          <p>descrição da casa</p>
+          <div className="aluguel">
+            <button className='Abtn'>Alugar</button>
+            <span className='Apreco'>R$ 325,00</span>
+          </div>
+        </div>
+        <div className="Acard">
+          <img src="src\assets\images\casa1.jpg" alt="imagem de casa" width={200}/>
+          <h3>Título da casa</h3>
+          <p>descrição da casa</p>
+          <div className="aluguel">
+            <button className='Abtn'>Alugar</button>
+            <span className='Apreco'>R$ 325,00</span>
+          </div>
+        </div>
+        <div className="Acard">
+          <img src="src\assets\images\casa2.jpg" alt="imagem de casa" width={200}/>
+          <h3>Título da casa</h3>
+          <p>descrição da casa</p>
+          <div className="aluguel">
+            <button className='Abtn'>Alugar</button>
+            <span className='Apreco'>R$ 325,00</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
