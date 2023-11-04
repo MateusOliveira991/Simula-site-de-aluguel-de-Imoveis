a4bcf670b22d86201ecda2885c34ff19e9a01fd4
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2023-11-04 13:40:45 -0300

    Feat: Criação do css da HeaderMain

--- reactgrupo3/src/components/HeaderMain/index.jsx
+++ reactgrupo3/src/components/HeaderMain/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './style.css';
 
 export default function HeaderMain() {
   return (
+<body>
+  
 
    <header>
     <div className='container' > 
@@ -28,5 +31,18 @@ export default function HeaderMain() {
     </div>
 
    </header>
+
+
+   <img className='homefoto' src="src\assets\homefoto.png" alt="homefoto" />
+
+    <p className='inicialtext' >Bem-vindo ao LocaSim!
+
+O LocaSim é um site de aluguel de imóveis que oferece uma variedade de opções para atender às suas necessidades. Seja você procurando um apartamento, uma casa ou um imóvel comercial para alugar, temos o que você procura. Clique e LocaSim</p>
+   <Link to="/Acomodacoes">  
+    <button className='acomodacoes' > Acomodações</button>
+    </Link>
+
+   </body>
+
   )
 }
