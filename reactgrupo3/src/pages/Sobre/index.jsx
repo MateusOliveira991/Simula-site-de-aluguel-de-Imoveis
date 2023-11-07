6e6619fa88d4e519918ad9353ee2cd3891933958
Joao Pedro <jpribeiroak@hotmail.com>
2023-11-07 11:14:10 -0300

    Feat:Criação do Sobre

--- reactgrupo3/src/pages/Sobre/index.jsx
+++ reactgrupo3/src/pages/Sobre/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 export default function Sobre() {
   return (
-    <div>
-      <h2>Aqui é a página sobre</h2>
+    <div className="container">
+      <h2>LocaSim</h2>
+      <h2>
+        <p>Bem-vindo ao mundo da LocaSim - onde a criatividade encontra a dedicação, e a inovação se une à tradição.</p></h2>
+          <h2>
+            <p>
+No coração da nossa empresa pulsam paixão e inovação. 
+Somos mais do que uma simples organização; 
+somos uma equipe de indivíduos apaixonados por fazer a diferença. No centro da nossa jornada está a busca pela excelência e o desejo de impactar positivamente o mundo que nos cerca.</p>
+          </h2>
+          <h2>Fundada em 2023, nossa empresa nasceu da visão ousada de um grupo de sonhadores determinados a transformar ideias em realidade. Ao longo dos anos, crescemos e evoluímos, mas nossa missão permanece a mesma: inspirar, capacitar e superar as expectativas.</h2>
+      <a className="nav-link" href="http://localhost:5173/acomodacoes">Ofertas</a>
+      <span className="separator">   </span> { }
+      <a className="nav-link" href="http://localhost:5173/contato">Suporte</a>
     </div>
-  )
+  );
 }
