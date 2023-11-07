0aef57964d56b1fea91f4d41744f991c9fffa576
Guilherme Pereira Mello <guiice3@gmail.com>
2023-11-07 16:36:44 -0300

    Feat: Funcionalidade de login do google.

--- reactgrupo3/src/App.jsx
+++ reactgrupo3/src/App.jsx
@@ -5,6 +5,20 @@ import AppRouter from "./routes/AppRouter"
 
 function App() {
 
+  // function handleCallbackResponse(response){
+  //   console.log("jwt" + response.credential)
+  // }
+
+  // useEffect(()=>{
+  //   google.accountes.id.initialize({
+  //     client_id:"338813839374-ij4r5ot709q6q7h3cgdfrdsuq797o582.apps.googleusercontent.com",
+  //     callback: handleCallbackResponse
+  //   })
+  //   google.accountes.id.renderButton(
+  //     document.getElementById("sigInDiv"),
+  //     {theme: "outline", size:"large"}
+  //   )
+  // },[])
 
   return (
     <>
@@ -12,7 +26,9 @@ function App() {
      <HeaderMain/> 
      <AppRouter/>
      </BrowserRouter>
+      {/* <div className="signInDiv">
 
+      </div> */}
     </>
   )
 }
--- reactgrupo3/src/components/HeaderMain/index.jsx
+++ reactgrupo3/src/components/HeaderMain/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './style.css';
+import GoogleLogin from 'react-google-login';
 
 export default function HeaderMain() {
+
+  const[profilePic, setProfilePic] = useState()
+  
+  const responseGoogle = (response)=>{
+    //  const{profileObj:{imgUrl}} = response
+    //  setProfilePic(imgUrl)
+    console.log(response)
+  }
 
   return ( 
 
@@ -28,6 +37,20 @@ export default function HeaderMain() {
     <Link to={"/contato"}> Contato </Link>  
 
     <Link to={"/acomodacoes"}> Acomodações </Link>  
+    <div className="container">
+    <div className='profilePic'>
+      <img src={profilePic} alt="" />
+    </div>
+    <div className="container">
+      <GoogleLogin
+      clientId='338813839374-ij4r5ot709q6q7h3cgdfrdsuq797o582.apps.googleusercontent.com'
+      buttonText='Logar'
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}/>
+    </div>
+    
+    </div>
+    
 
     </div>
     </div>
